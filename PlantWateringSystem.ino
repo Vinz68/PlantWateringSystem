d69d939bab1bd2fded1956e82dc954aa1226c68a
Vincent van Beek <vin.van.beek@outlook.com>
2021-03-11 20:49:36 +0100

    function for serial monitor & fix typos

--- PlantWateringSystem.ino
+++ PlantWateringSystem.ino
@@ -13,49 +13,51 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Plant_watering_system Setup");
 
-  // Analog input for moister sensor (0 for wet and 1023 for dry)
+  // Analog input for moisture sensor (0 for wet and 1023 for dry)
   pinMode(ANA, INPUT);
 
-  // Digital input moister sensor
-  // LOW for wet and HIGH for dry
+  // Digital input moisture sensor (LOW for wet and HIGH for dry)
   pinMode(DIGI, INPUT);
 
-  // Output to drive the motor (onn/off)
+  // Output to drive the motor (on/off)
   pinMode(POMPOUT, OUTPUT);
 
 }
 void loop() {
-
-  // Read moister sensor values (analog and digital inputs)
+  // Read moisture sensor values (analog and digital inputs)
   analogValue = analogRead(ANA);
   digitalValue = digitalRead(DIGI);
 
-  // Serial data
-  Serial.print("Analog raw: ");
-  Serial.println(analogValue);
-  Serial.print("Digital raw: ");
-  Serial.println(digitalValue);
-  Serial.println(" ");
+  outputToSerialMonitor();
 
   // 0 for wet and 1023 for dry
   if (analogValue > 800) {
     if (counter == 0)
-      waterPlantLittleBit();
+      dripThePlant();
     else
-        counter--;
-    Serial.print("counter: ");
-    Serial.println(counter);
+      counter--;
   } 
-
   delay(1000);
 }
 
-
-void waterPlantLittleBit() {
+// Enable motor for short time, so little bit of water will reach the plant
+// Might need to change the delay and/or counter to finetune to your system
+void dripThePlant() {
     Serial.println("Pump a little bit");
     digitalWrite(POMPOUT, true);
     delay(750);
     counter = 25;
     Serial.println("Pump off");
     digitalWrite(POMPOUT, false);
+}
+
+void outputToSerialMonitor() {
+  // Data for Tools->Serial Monitor
+  Serial.print("Analog raw: ");
+  Serial.println(analogValue);
+  Serial.print("Digital raw: ");
+  Serial.println(digitalValue);
+  Serial.println(" ");
+  Serial.print("counter: ");
+  Serial.println(counter);
 }
